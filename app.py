830e088bee5273905850ab635e255723cc5b21dc
Mouhamed Doumbia <doumbiasoft@gmail.com>
2023-02-13 21:02:56 -0500

    remove last commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template, redirect, flash, session
+from flask import Flask,request,render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,8 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] ="IamMouhamedDoumbia86"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
-
-responses_session = "responses"
+responses =[]
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -16,17 +15,17 @@ def page_not_found(e):
 
 @app.route('/')
 def index():
+    responses.clear()
     return render_template('index.html',survey=satisfaction_survey)
 
 @app.route("/start", methods=["POST"])
 def start():
-    session[responses_session]=[]
     return redirect("/questions/0")
 
 
 @app.route('/questions/<id>')
 def get_questions(id):
-    responses = session.get(responses_session)
+
     questions_size=len(satisfaction_survey.questions)
     responses_size=len(responses)
 
@@ -58,10 +57,10 @@ def get_questions(id):
 
 @app.route("/answer", methods=["POST"])
 def get_answer():
-    responses = session.get(responses_session)
+  
     answer = request.form.get('answer')
     responses.append(answer)
-    session[responses_session]= responses
+
     questions_size=len(satisfaction_survey.questions)
     responses_size=len(responses)
 
@@ -72,7 +71,6 @@ def get_answer():
     
 @app.route('/complete')
 def end():
-    responses = session.get(responses_session)
     questions_size=len(satisfaction_survey.questions)
     return render_template('complete.html',questions=satisfaction_survey.questions,responses=responses,size=questions_size)
 
